94db72706228f23811067f2f40e60ef602b1bba7
GutNick <chelyabinsk.it@gmail.com>
2022-01-15 21:42:51 +0500

    Финальная сборка v9.0

--- README.md
+++ README.md
@@ -4,10 +4,11 @@
 Обучающий проект от **Яндекс практикума**. Написанный на языке разметки HTML с приминением стилей CSS и языка программирования JavaScript. Файловая структура соответствует правилам Nested БЭМ. Проект призван оценить мои навыки веб разработки, после прохождения 4,5,6,7 и 8 спринтов. В частности использования скриптов js.
 
 ## Update
-После прохождения 8 спринта реализованы принципы ООП в создании классов. А также проект собирается с помощью модуля webpack.
+После прохождения 9 спринта реализована работа с сервером
 
 ## Ссылки
 1. [Макет Figma 4 спринт](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4)
 2. [Макет Figma 5 спринт](https://www.figma.com/file/bjyvbKKJN2naO0ucURl2Z0/JavaScript.-Sprint-5)
 3. [Макет Figma 6 спринт](https://www.figma.com/file/kRVLKwYG3d1HGLvh7JFWRT/JavaScript.-Sprint-6)
+4. [Макет Figma 9 спринт](https://www.figma.com/file/PSdQFRHoxXJFs2FH8IXViF/JavaScript.-Sprint-9)
 5. [Url проекта на github.io](https://gutnick.github.io/mesto/)
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,6 +8,9 @@ export const elementAddButton = document.querySelector('.profile__button-add');
 export const formPopupProfile = popupProfile.querySelector('.popup__form');
 export const formPopupAddElement = popupAddElement.querySelector('.popup__form');
 export const inputList = document.querySelector('.popup_profile').querySelectorAll('.popup__field');
+export const buttonOpenChangeAvatar = document.querySelector('.profile__wrapper-avatar');
+export const popupAvatar = document.querySelector('.popup_avatar');
+export const formPopupAvatar = popupAvatar.querySelector('.popup__form');
 
 export const config = {
   formSelector: '.popup__form',
@@ -15,29 +18,3 @@ export const config = {
   submitButtonSelector: '.popup__button',
   inputErrorClass: 'popup__field_type_error',
 }
-export const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-  ];
